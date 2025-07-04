7cfd4775e259b684c6e704cdc97a59f7912d56b7
siso5 <97729185+siso5@users.noreply.github.com>
2025-07-04 09:55:49 +0900

    work-06-day2

--- ip.c
+++ ip.c
@@ -1,15 +1,155 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #include "util.h"
 #include "net.h"
 #include "ip.h"
 
+
+const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
+const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
+
+
+struct ip_hdr {
+    uint8_t vhl;
+    uint8_t tos;
+    uint16_t total;
+    uint16_t id;
+    uint16_t offset;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t sum;
+    ip_addr_t src;
+    ip_addr_t dst;
+    uint8_t options[];
+};
+
+int
+ip_addr_pton(const char *p, ip_addr_t *n)
+{
+    char *sp, *ep;
+    int idx;
+    long ret;
+
+    sp = (char *)p;
+    for (idx = 0; idx < 4; idx++) {
+        ret = strtol(sp, &ep, 10);
+        if (ret < 0 || ret > 255) {
+            return -1;
+        }
+        if (ep == sp) {
+            return -1;
+        }
+        if ((idx == 3 && *ep != '\0') || (idx != 3 && *ep != '.')) {
+            return -1;
+        }
+        ((uint8_t *)n)[idx] = ret;
+        sp = ep + 1;
+    }
+    return 0;
+}
+
+
+char *
+ip_addr_ntop(ip_addr_t n, char *p, size_t size)
+{
+    uint8_t *u8;
+
+    u8 = (uint8_t *)&n;
+    snprintf(p, size, "%d.%d.%d.%d", u8[0], u8[1], u8[2], u8[3]);
+    return p;
+}
+
+
+static void
+ip_dump(const uint8_t *data, size_t len)
+{
+        struct ip_hdr *hdr;
+    uint8_t v, hl, hlen;
+    uint16_t total, offset;
+    char addr[IP_ADDR_STR_LEN];
+
+    flockfile(stderr);
+    hdr = (struct ip_hdr *)data;
+    v = (hdr->vhl & 0xf0) >> 4;
+    hl = hdr->vhl & 0x0f;
+    hlen = hl << 2;
+    fprintf(stderr, "        vhl: 0x%02x [v: %u, hl: %u (%u)]\n", hdr->vhl, v, hl, hlen);
+    fprintf(stderr, "        tos: 0x%02x\n", hdr->tos);
+    total = ntoh16(hdr->total);
+    fprintf(stderr, "      total: %u (payload: %u)\n", total, total - hlen);
+    fprintf(stderr, "         id: %u\n", ntoh16(hdr->id));
+    offset = ntoh16(hdr->offset);
+    fprintf(stderr, "     offset: 0x%04x [flags=%x, offset=%u]\n", offset, (offset & 0xe000) >> 13, offset & 0x1fff);
+    fprintf(stderr, "        ttl: %u\n", hdr->ttl);
+    fprintf(stderr, "   protocol: %u\n", hdr->protocol);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    fprintf(stderr, "        src: %s\n", ip_addr_ntop(hdr->src, addr, sizeof(addr)));
+    fprintf(stderr, "        dst: %s\n", ip_addr_ntop(hdr->dst, addr, sizeof(addr)));
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
+
+}
+
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
-    debugf("dev=%s, len=%zu", dev->name, len);
-    debugdump(data, len);
+    struct ip_hdr *hdr;
+    uint8_t v;
+    uint16_t hlen, total, offset;
+
+    if (len < IP_HDR_SIZE_MIN) {
+        errorf("too short");
+        return;
+    }
+    hdr = (struct ip_hdr *)data;
+
+    /* Execise 6-1 */
+    //(1) IPヘッダのバージョンをチェックし、IPv4でない場合はエラーを返す
+    v = (hdr->vhl & 0xf0) >> 4;
+    if (v != IP_VERSION_IPV4) {
+        errorf("not ipv4");
+        return;
+    }
+
+    //(2) IPヘッダのヘッダ長を取得し、入力データの長さ（len）がヘッダ長より小さい場合はエラーメッセージを出力して中断を返す
+    hlen = (hdr->vhl & 0x0f) << 2;
+    if (len < hlen) {
+        errorf("invalid header length");
+        return;
+    }
+
+    //(3) IPヘッダの全体の長さを取得し、入力データの長さ（len）が全体の長さより小さい場合はエラーメッセージを出力して中断を返す
+    total = ntoh16(hdr->total);
+    if (len < total) {
+        errorf("invalid total length");
+        return;
+    }
+
+    //(4) cksum16() での検証に失敗した場合はエラーメッセージを出力して中断
+    if (cksum16((uint16_t *)data, hlen, 0) != 0) {
+        errorf("invalid checksum");
+        return;
+    }
+
+    /* ------------*/
+
+
+    offset = ntoh16(hdr->offset);
+    if (offset & 0x2000 || offset & 0x1fff) {
+        errorf("fragments does not support");
+        return;
+    }
+    debugf("dev=%s, protocol=%u, total=%u", dev->name, hdr->protocol, total);
+    ip_dump(data, total);
+
+
+    
+
 
 }
 
